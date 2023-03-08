66ba58e75f2ca8fd5e297a804c09ebb5489e9830
_DG_ <damirgaynanov@gmail.com>
2023-03-08 23:58:34 +0300

    Second commit_Working version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача:
+// Написать программу, которая из имеющигося массива строк  формирует массив из строк, 
+// длина которых меньше либо равно 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// оучге рьрйиист исключительно массивами
+
+string[] arrText = { "Hello", "Hi", "By" };
+PrintArray(arrText);
+
+void PrintArray(string[] arrayText)
+{
+    for (int i = 0; i < arrayText.Length; i++)
+    {
+        string str = arrayText[i];
+        if (str.Length < 3) Console.Write($"{arrayText[i]}, ");
+    }
+}
+
+
+
+// Console.WriteLine("Input Text 1");
+// string text1 = Console.ReadLine();
+// Console.WriteLine("Input Text 2");
+// string text2 = Console.ReadLine();
+// Console.WriteLine("Input Text 3");
+// string text3 = Console.ReadLine();
