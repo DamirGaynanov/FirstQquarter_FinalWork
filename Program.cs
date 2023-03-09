0adb4683fa69800435d5c95a8f1ab556af4f6f75
_DG_ <damirgaynanov@gmail.com>
2023-03-10 01:22:34 +0300

    Corrected

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 // длина которых меньше либо равно 3 символа. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // оучге рьрйиист исключительно массивами
+
 Console.WriteLine("Input Text 1");
 string text1 = Console.ReadLine() ?? "";
 Console.WriteLine("Input Text 2");
@@ -12,24 +13,39 @@ string text3 = Console.ReadLine() ?? "";
 Console.WriteLine("Input Text 4");
 string text4 = Console.ReadLine() ?? "";
 
-string[] arrText = { text1, text2, text3, text4 };
-PrintArray(arrText);
-
-
-void PrintArray(string[] arrayText)
+string[] array1 = new string[] { text1, text2,  text3, text4};
+string[] array2 = new string[array1.Length];
+arraySelective(array1, array2);
+Console.Write($"[");
+for (int i = 0; i < array1.Length; i++)
 {
-    Console.Write($"[{text1}, {text2}, {text3}, {text4}] -> [");
-    for (int i = 0; i < arrayText.Length; i++)
+    if (i < array1.Length-1) Console.Write($"{array1[i]}, ");
+    else Console.Write($"{array1[i]}] -> ");
+}
+PrintArray(array2);
+
+void arraySelective(string[] arr1, string[] arr2)
+{ 
+    int j = 0;
+    for (int i = 0; i < arr1.Length; i++)
     {
-        string str = arrayText[i];
-        if (i < arrayText.Length - 2)
+        string str = arr1[i];
+        if (str.Length <= 3) 
         {
-            if (str.Length <= 3) Console.Write($"{arrayText[i]},");
-        }
-        else
-        {
-            if (str.Length <= 3) Console.Write($"{arrayText[i]}");
+            arr2[j] = arr1[i];
+            j++;
         }
     }
-    Console.Write("]");
+}
+
+void PrintArray(string[] array)
+{
+
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 2) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write($"]");
 }
